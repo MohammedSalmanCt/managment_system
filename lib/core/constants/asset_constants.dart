d3e88d781236d0106e95f89236410eb0cd76cafd
salman <ctsalmanct@gmail.com>
2024-06-13 17:23:48 +0530

    commit

--- lib/core/constants/asset_constants.dart
+++ lib/core/constants/asset_constants.dart
@@ -1,6 +1,5 @@
 class AssetConstants {
   static const loginImg = 'assets/loginScreen/login 1.png';
   static const logo = 'assets/logo/Logo.svg';
-  static const log1 = 'assets/logo/Logo.svg';
 
 }
